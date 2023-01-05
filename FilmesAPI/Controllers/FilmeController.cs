23d7ae75455ef36898f6de2387aab15e4a19938f
vitormerola <vitor.merola@prevhab.com.br>
2023-01-05 13:59:28 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,45 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+// O  [Route("[controller]")] serve para pegar o nome do controlador e colocar na rota. O controller escrito dessa maneira no route diz para ele.
+//copiar o nome da classe de controller para trás, então com a classe com nome FilmeController, ele copiou Filme e colocou.
+// dentro do route, ficando, "por debaixo dos panos", dessa maneira: [Route("[Filme]")].
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+    
+    [HttpPost]
+    // O FromBody quer dizer que a informação vem através do corpo da requisição.
+    // Estudar mais sobre CreatedAtAction - https://www.macoratti.net/19/06/aspnc_3dwebapi1.htm
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id}, filme);
+
+    }
+
+    [HttpGet]
+    // Recupera os dados inseridos pelo AdicionaFilme, que no caso é um HttpPost.
+    // Para Explicitar que o usuário vai passar os valores de skip e take através de uma consulta, utilizei o FromQuery
+    // O skip está = 0 para caso o usuário não passar nenhum valor ele ser 0 e não pular nada
+    // O take está =50, pois foi um valor que julguei que não seria muito exagerado e para não sobrecarregar a memória sistema de filmes
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId (int id)
+    {
+      var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
+
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+// O required é quando o preenchimento do campo é obrigatório, MaxLength é para limitar o número de caracteres, Range é para definir o valor mínimo e máximo.
+// No StringLenth da para colocar o mínimo e o máximo de caracteres.
+// Diferença entre StringLenth e MaxLenth - https://social.msdn.microsoft.com/Forums/en-US/95d484d5-9592-4678-8a57-4483689f27ec/what-is-difference-between-stringlength-and-maxlength-attribute?forum=aspadoentitylinq
+// Para sermos mais expressivos com quem for consumir a API, podemos deixar mensagens mais claras de erro através do parâmetro ErrorMessage.
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    //[StringLength(50, ErrorMessage = "O tamanho do gênero tem de estar entre 1 e 50 caracteres.", MinimumLength = 1)]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
